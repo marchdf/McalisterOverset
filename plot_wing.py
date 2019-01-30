7d6f80e935a2a8535e2fea903bea9a92953e240f
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2019-01-30 08:23:29 -0700

    Added wind post-processing scripts.

---
+++ plot_wing.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+# ========================================================================
+#
+# Imports
+#
+# ========================================================================
+import argparse
+import sys
+import os
+import glob
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+import pandas as pd
+import scipy.interpolate as spi
+import yaml
+
+# ========================================================================
+#
+# Some defaults variables
+#
+# ========================================================================
+plt.rc("text", usetex=True)
+cmap_med = [
+    "#F15A60",
+    "#7AC36A",
+    "#5A9BD4",
+    "#FAA75B",
+    "#9E67AB",
+    "#CE7058",
+    "#D77FB4",
+    "#737373",
+]
+cmap = [
+    "#EE2E2F",
+    "#008C48",
+    "#185AA9",
+    "#F47D23",
+    "#662C91",
+    "#A21D21",
+    "#B43894",
+    "#010202",
+]
+dashseq = [
+    (None, None),
+    [10, 5],
+    [10, 4, 3, 4],
+    [3, 3],
+    [10, 4, 3, 4, 3, 4],
+    [3, 3],
+    [3, 3],
+]
+markertype = ["s", "d", "o", "p", "h"]
+
+
+# ========================================================================
+#
+# Function definitions
+#
+# ========================================================================
+def get_merged_csv(fnames, **kwargs):
+    lst = []
+    for fname in fnames:
+        try:
+            df = pd.read_csv(fname, **kwargs)
+            lst.append(df)
+        except pd.errors.EmptyDataError:
+            pass
+    return pd.concat(lst, ignore_index=True)
+
+
+def parse_ic(fname):
+    """Parse the Nalu yaml input file for the initial conditions"""
+    with open(fname, "r") as stream:
+        try:
+            dat = yaml.load(stream)
+            u0 = float(
+                dat["realms"][0]["initial_conditions"][0]["value"]["velocity"][0]
+            )
+            rho0 = float(
+                dat["realms"][0]["material_properties"]["specifications"][0]["value"]
+            )
+            mu = float(
+                dat["realms"][0]["material_properties"]["specifications"][1]["value"]
+            )
+
+            return u0, rho0, mu
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def sort_by_angle(x, y, var):
+    """Radial sort of variable on x and y for plotting
+
+    Inspired from:
+    http://stackoverflow.com/questions/35606712/numpy-way-to-sort-out-a-messy-array-for-plotting
+
+    """
+
+    # Get the angle wrt the mean of the cloud of points
+    x0, y0 = x.mean(), y.mean()
+    angle = np.arctan2(y - y0, x - x0)
+
+    # Sort based on this angle
+    idx = angle.argsort()
+    idx = np.append(idx, idx[0])
+
+    return x[idx], y[idx], var[idx]
+
+
+# ========================================================================
+#
+# Main
+#
+# ========================================================================
+if __name__ == "__main__":
+
+    # ========================================================================
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="A simple plot tool")
+    parser.add_argument("-s", "--show", help="Show the plots", action="store_true")
+    args = parser.parse_args()
+
+    # ========================================================================
+    # Setup
+    ninterp = 100
+
+    fdir = os.path.abspath("DES")
+    yname = os.path.join(fdir, "mcalister.yaml")
+    fname = "avg_slice.csv"
+    sdirs = ["wing_slices"]
+    labels = ["DES"]
+
+    # simulation setup parameters
+    u0, rho0, mu = parse_ic(yname)
+    chord = 1
+
+    # ========================================================================
+    # Loop on data directories
+    for i, sdir in enumerate([os.path.join(fdir, sdir) for sdir in sdirs]):
+
+        # ========================================================================
+        # Read in data
+        df = pd.read_csv(os.path.join(sdir, fname), delimiter=",")
+        renames = {
+            "Points:0": "x",
+            "Points:1": "y",
+            "Points:2": "z",
+            "pressure": "p",
+            "iblank": "iblank",
+            "absIBlank": "absIBlank",
+            "pressure_force_:0": "fpx",
+            "pressure_force_:1": "fpy",
+            "pressure_force_:2": "fpz",
+            "tau_wall": "tau_wall",
+            "velocity_:0": "ux",
+            "velocity_:1": "uy",
+            "velocity_:2": "uz",
+            "time": "avg_time",
+        }
+        df.columns = [renames[col] for col in df.columns]
+
+        # Calculate the negative of the surface pressure coefficient
+        df["cp"] = -df["p"] / (0.5 * rho0 * u0 ** 2)
+
+        # ========================================================================
+        # Plot cp in each slice
+        zslices = np.unique(df["z"])
+
+        for k, zslice in enumerate(zslices):
+            subdf = df[df["z"] == zslice]
+            print(subdf)
+
+            # Sort for a pretty plot
+            x, y, cp = sort_by_angle(
+                subdf["x"].values, subdf["y"].values, subdf["cp"].values
+            )
+
+            # plot
+            plt.figure(k)
+            ax = plt.gca()
+            p = plt.plot(x / chord, cp, ls="-", lw=2, color=cmap[i], label=labels[i])
+            p[0].set_dashes(dashseq[i])
+
+    # ========================================================================
+    # Save plots
+    fname = "wing_cp.pdf"
+    with PdfPages(fname) as pdf:
+
+        for k, zslice in enumerate(zslices):
+            plt.figure(k)
+            ax = plt.gca()
+            plt.xlabel(r"$x/c$", fontsize=22, fontweight="bold")
+            plt.ylabel(r"$-c_p$", fontsize=22, fontweight="bold")
+            plt.setp(ax.get_xmajorticklabels(), fontsize=16, fontweight="bold")
+            plt.setp(ax.get_ymajorticklabels(), fontsize=16, fontweight="bold")
+            plt.xlim([0, chord])
+            plt.ylim([-1.5, 4.5])
+            ax.set_title(r"$z={0:.5f}$".format(zslice))
+            plt.tight_layout()
+            if k == 1:
+                legend = ax.legend(loc="best")
+            pdf.savefig(dpi=300)
+
+    if args.show:
+        plt.show()
---
+++ pp_wing.py
@@ -0,0 +1,105 @@
+#
+# Run this script on NERSC Edison with something like:
+#    start_pvbatch.sh 4 4 00:10:00 default debug `pwd`/pp_wing.py
+#
+
+
+# ----------------------------------------------------------------
+# imports
+# ----------------------------------------------------------------
+# import the simple module from the paraview
+from paraview.simple import *
+
+# disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+import os
+import glob
+import shutil
+
+# ----------------------------------------------------------------
+# setup
+# ----------------------------------------------------------------
+
+# Get file names
+fdir = os.path.abspath("./DES/out")
+pattern = "*.e.*"
+fnames = sorted(glob.glob(os.path.join(fdir, pattern)))
+
+odir = os.path.abspath("./DES/wing_slices")
+shutil.rmtree(odir, ignore_errors=True)
+os.makedirs(odir)
+oname = os.path.join(odir, "output.csv")
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'ExodusIIReader'
+exoreader = ExodusIIReader(FileName=fnames)
+exoreader.PointVariables = [
+    "iblank",
+    "pressure",
+    "pressure_force_",
+    "tau_wall",
+    "velocity_",
+]
+exoreader.NodeSetArrayStatus = []
+exoreader.SideSetArrayStatus = ["wing"]
+exoreader.ElementBlocks = []
+
+# get active view
+renderView1 = GetActiveViewOrCreate("RenderView")
+
+# create a new 'Calculator'
+calculator1 = Calculator(Input=exoreader)
+calculator1.ResultArrayName = "absIBlank"
+calculator1.Function = "abs(iblank)"
+
+# create a new 'Threshold'
+threshold1 = Threshold(Input=calculator1)
+threshold1.Scalars = ["POINTS", "absIBlank"]
+threshold1.ThresholdRange = [1.0, 1.0]
+
+# wing properties
+wing_length = 3.3
+
+# create a new 'Slice'
+# at span location corresponding to McAlister paper Fig 6.
+# [0.994, 0.984, 0.974, 0.974, 0.959, 0.944, 0.899, 0.843, 0.773, 0.692, 0.597, 0.491, 0.370, 0.238, 0.094]
+slice1 = Slice(Input=threshold1)
+slice1.SliceType = "Plane"
+slice1.SliceOffsetValues = [
+    0.0,
+    0.0198,
+    0.0528,
+    0.0858,
+    0.0858,
+    0.1353,
+    0.1848,
+    0.3333,
+    0.5181,
+    0.7491,
+    1.0164,
+    1.3299,
+    1.6797,
+    2.079,
+    2.5146,
+    2.9898,
+]
+
+# init the 'Plane' selected for 'SliceType'
+slice1.SliceType.Origin = [0.0, 0.0, 0.0]
+slice1.SliceType.Normal = [0.0, 0.0, 1.0]
+
+# ----------------------------------------------------------------
+# save data
+# ----------------------------------------------------------------
+SaveData(
+    oname,
+    proxy=slice1,
+    Precision=5,
+    UseScientificNotation=0,
+    WriteTimeSteps=1,
+    FieldAssociation="Points",
+)
