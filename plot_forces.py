Fixed cp/cl calculation

plot_forces.py
@@ -75,20 +75,13 @@ if __name__ == "__main__":
     area = 1.0 * 6.6
     u0, v0, w0, umag0, rho0, mu = utilities.parse_ic(yname)
     dynPres = rho0 * 0.5 * (umag0 ** 2)
-
-    # Rotations
-    aoa, R = utilities.parse_angle(args.folder)
-    for k in range(df.shape[0]):
-        Fp_rot = np.dot(R, (df.Fpx.iloc[k], df.Fpy.iloc[k]))
-        Fv_rot = np.dot(R, (df.Fvx.iloc[k], df.Fvy.iloc[k]))
-        df.Fpx.iloc[k] = Fp_rot[0]
-        df.Fpy.iloc[k] = Fp_rot[1]
-        df.Fvx.iloc[k] = Fv_rot[0]
-        df.Fvy.iloc[k] = Fv_rot[1]
+    aoa, baseline_aoa = utilities.parse_angle(args.folder)
 
     # Lift and drag
-    df["cl"] = (df.Fpy + df.Fvy) / (dynPres * area)
-    df["cd"] = (df.Fpx + df.Fvx) / (dynPres * area)
+    alpha = np.radians(baseline_aoa - aoa)
+    c, s = np.cos(alpha), np.sin(alpha)
+    df["cl"] = ((df.Fpy + df.Fvy) * c + (df.Fpx + df.Fvx) * s) / (dynPres * area)
+    df["cd"] = (-(df.Fpy + df.Fvy) * s + (df.Fpx + df.Fvx) * c) / (dynPres * area)
 
     # Experimental values
     edir = os.path.abspath(os.path.join("exp_data", f"aoa-{aoa}"))

utilities.py
@@ -57,12 +57,7 @@ def parse_ic(fname):
 # ========================================================================
 def parse_angle(fdir):
     """Parse the angle of attack from the folder name"""
-    aoa = int(re.search(r"\d+", fdir).group())
-    baseline_aoa = 12.0
-    theta = np.radians(aoa - baseline_aoa)
-    c, s = np.cos(theta), np.sin(theta)
-    R = np.array(((c, -s), (s, c)))
-    return aoa, R
+    return int(re.search(r"\d+", fdir).group()), 12.0
 
 
 # ========================================================================